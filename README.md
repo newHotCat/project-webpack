24fc03d8e8027ff2cadb3d00ef880df85386fe97
jiantong.liu <liujiantongvip@163.com>
2018-03-12 16:21:49 +0800

    管理资源

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # webpack 
+——————————————————————————
 ## 起步
     1. npx webpack src/index.js --output dist/bundle.js   执行 npx webpack，会将我们的脚本作为入口起点，然后输出为 bundle.js
     2. webpack --config webpack.config.js 
+## 资源管理
+    1. 加载css   style-loader css-loader
+    2. 加载图片  file-loader 合乎逻辑下一步是，压缩和优化你的图像。查看 image-webpack-loader 和 url-loader，以了解更多关于如果增强加载处理图片功能。
+    3. 加载字体   file-loader  没有测试
+    4. 加载数据   eg: data.xml   xml-loader   
+      - 在使用 d3 等工具来实现某些数据可视化时，预加载数据会非常有用。我们可以不用再发送 ajax 请求，然后于运行时解析数据，而是在构建过程中将其提前载入并打包到模块中，以便浏览器加载模块后，可以立即从模块中解析数据。
+    5. 全局资源 未理解
+      - 
+    6. 
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,41 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
   }
 };
+
+/**
+ * webpack 根据正则表达式，来确定应该查找哪些文件，并将其提供给指定的 loader。在这种情况下，以 .css 结尾的全部文件，都将被提供给 style-loader 和 css-loader。
+ * 这使你可以在依赖于此样式的文件中 import './style.css'。现在，当该模块运行时，含有 CSS 字符串的 <style> 标签，将被插入到 html 文件的 <head> 中。
+ **/
